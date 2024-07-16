edebf1f971e1e9c71dcec02622167fbed963e246
2akia <2akssultana@gmail.com>
2024-07-16 12:17:44 +0100

    Create index.php
    created index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="reset.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <
+    <title>Document</title>
+</head>
+<body>
+
+<nav>
+
+<div class="wrapper">
+<a href="index.php">img src="img/logo-white.png" alt="Blogs logo"></a>
+
+<ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="discover.php">About Us</a></li>
+    <li><a href="blog.php">Find Blogs</a></li>
+    <li><a href="signup.php">Sign Up</a></li>
+    <li><a href="login.php">Log In</a></li>
+</ul>
+</div>
+</nav>
+    
+<div class="wrapper">
+
+<section class="index-intro">
+    <h1>This is an Introduction</h1>
+    <p>This is a Paragraph</p>
+</section>
+
+
+</div>
+
+
+</body>
+</html>
